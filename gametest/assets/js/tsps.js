9a80e89c98485955ce438c73cf5f69ac470c82ea
edgarravenhorst <edgar@code.rehab>
2016-03-12 18:16:23 +0100

    changed setup

--- gametest/assets/js/tsps.js
+++ gametest/assets/js/tsps.js
@@ -18,34 +18,4 @@ var GameTSPS = function(local){
   this.connection.onPersonMoved = function(data){}
   this.connection.onPersonUpdated = function(data){}
   this.connection.onPersonLeft = function(data){}
-
 }
-
-if(typeof Game != "undefined")
-  Game.prototype.tsps = {
-
-    follow: function(stageObj){
-      tsps.connection.onPersonUpdated = function(data){
-        console.log(stageObj, data.boundingrect.x - data.boundingrect.width/2 * 1024,  data.boundingrect.y - data.boundingrect.height/2 * 768)
-        stageObj.stageX = data.boundingrect.x * 1024
-        stageObj.stageY = data.boundingrect.y * 768
-      }
-    },
-
-    catapultAction: function(onCompleteFunc) {
-      var velocity, angle;
-      //get start point
-      //keep an aye out for onleave
-      //calculate velocity, angle, etc
-      //complete Action
-      onCompleteFunc(velocity, angle);
-    },
-
-    drawLine: function(brush, stage) {
-      tsps.connection.onPersonUpdated = function(data){
-        var circle = new createjs.Shape();
-        circle.graphics.beginFill("DeepSkyBlue").drawCircle(data.boundingrect.x * 1024,  data.boundingrect.y * 768, 10);
-        stage.addChild(circle);
-      }
-    }
-  }
