9206e5eee7a52fafaaed6fb4d8c0a76a8ea6e726
Heinze Havinga <heinze.havinga@gmail.com>
2016-03-13 18:55:19 +0100

    added colors

--- src/hackathon/TSPS/src/draw.js
+++ src/hackathon/TSPS/src/draw.js
@@ -2,9 +2,12 @@
 
 
 GameTSPS.prototype.draw = function(brush, stage) {
+    var colors = ["#E81300","#FFFA00","#00FF1F","#1700E8","#FF00B5"];
   this.connection.onPersonUpdated = function(data){
+
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(data.boundingrect.x * 1024,  data.boundingrect.y * 768, 10);
+                var fill = colors[data.id % (colors.length-1)];
+    circle.graphics.beginFill(fill).drawCircle(data.boundingrect.x * 1024,  data.boundingrect.y * 768, 10);
     this.stage.addChild(circle);
   }.bind(this);
 }
